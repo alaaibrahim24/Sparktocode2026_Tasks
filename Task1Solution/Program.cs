583b7839a93b9c5175c527dbb0dc4a19148f06dd
alaaibrahim24 <alaaibrahimal@icloud.com>
2026-07-06 01:57:49 +0400

    i solved  Task 1: Personal Info Card

--- Task1Solution/Program.cs
+++ Task1Solution/Program.cs
@@ -4,14 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
 
 
 
+            //Task 1: Personal Info Card
+
+            string name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+            Console.WriteLine($"Name: {name}\nAge: {age}\nHeight: {height}\nStudent: {isStudent}");
 
 
-
-
+            ///////////////////////////////////////////////////////////
+            ///
 
 
 
