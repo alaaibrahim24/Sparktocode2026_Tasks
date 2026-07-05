04fc027a73d4207e450c6988b8152d81e6d2504f
alaaibrahim24 <alaaibrahimal@icloud.com>
2026-07-06 01:56:07 +0400

    Add Project File

---
+++ Task1Solution/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task1Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
