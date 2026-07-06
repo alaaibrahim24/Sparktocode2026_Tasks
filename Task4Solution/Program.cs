e257179b562ff4ed6d9cd9d7504081573b190e09
alaaibrahim24 <alaaibrahimal@icloud.com>
2026-07-06 22:40:41 +0400

    implementing Task 7 (GetGradeLetter)

--- Task4Solution/Program.cs
+++ Task4Solution/Program.cs
@@ -49,6 +49,19 @@
             //////////////////////////////////////////////////////
             ///
 
+            //Task 7 (GetGradeLetter)
+
+            static string GetGradeLetter(int score)
+            {
+                if (score >= 90) return "A";
+                else if (score >= 80) return "B";
+                else if (score >= 70) return "C";
+                else if (score >= 60) return "D";
+                else return "F";
+            }
+            ///////////////////////////////////////////////////////
+            ///
+
 
 
 
